84de5d8af5a1ac152929bfe8d5d47e6c3aa57c00
Dmitrij Koniajev <dimchansky@gmail.com>
2018-09-05 12:16:01 +0300

    added solc configuration in truffle.js

--- truffle.js
+++ truffle.js
@@ -13,6 +13,13 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    }
 };
