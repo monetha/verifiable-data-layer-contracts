ebcc64c37a26b3df35b6e9c34bf5982f42a38e23
Dmitrij Koniajev <dimchansky@gmail.com>
2019-04-17 13:06:12 +0300

    TODO added

--- contracts/storage/Storage.sol
+++ contracts/storage/Storage.sol
@@ -99,7 +99,7 @@ contract Storage is ClaimableProxy
         uint256 stateExpired;           // The state expiration timestamp
     }
 
-    uint internal openPrivateDataExchangeCount; // the count of open private data exchanges
+    uint internal openPrivateDataExchangeCount; // the count of open private data exchanges TODO: use it in contract destruction/ownership transfer logic
     PrivateDataExchange[] public privateDataExchanges;
 
     /***************************************************************************
