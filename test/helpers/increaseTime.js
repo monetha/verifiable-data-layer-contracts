076326a2f65699c3aaefa632add4aa0c24597321
Dmitrij Koniajev <dimchansky@gmail.com>
2019-04-18 14:06:52 +0300

    added duration functions

--- test/helpers/increaseTime.js
+++ test/helpers/increaseTime.js
@@ -1,4 +1,4 @@
-module.exports.increaseTime = function increaseTime(duration) {
+function increaseTime(duration) {
     const id = Date.now();
 
     return new Promise((resolve, reject) => {
@@ -25,4 +25,18 @@ module.exports.increaseTime = function increaseTime(duration) {
             }
         );
     });
+}
+
+const duration = {
+    seconds: function(val) { return val},
+    minutes: function(val) { return val * this.seconds(60) },
+    hours:   function(val) { return val * this.minutes(60) },
+    days:    function(val) { return val * this.hours(24) },
+    weeks:   function(val) { return val * this.days(7) },
+    years:   function(val) { return val * this.days(365)}
+};
+
+module.exports = {
+    increaseTime,
+    duration
 };
